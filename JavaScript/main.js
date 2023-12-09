c1df47a907c37efe248cf4618fdf3437c9b9e139
Patryk Szczemczak <patrykszczemczak@wp.pl>
2023-12-09 16:59:23 +0100

    prawie skończona apka

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,36 @@
-console.log('js działa');
+const input=document.getElementById('inputBox');
+const buttonAdd=document.querySelector('.btn');
+const buttonClear=document.querySelector('.btn--clear')
+const list=document.querySelector('.ulList ul');
+
+
+buttonAdd.addEventListener('click',function(){
+    const value=input.value;
+    if(value=='') alert('Nic nie napisaleś!')
+    else{
+    const liItem=document.createElement('li');
+    liItem.innerText=value; 
+    list.appendChild(liItem);
+    }
+    let liItems=[...document.querySelectorAll('.ulList ul li')];
+    liItems.forEach(function(item){
+        item.addEventListener('click',function(){
+            item.classList.toggle('checked');
+            item.removeEventListener('click',function(){
+                item.classList.toggle('checked');
+            })
+        })
+    })
+    })
+
+buttonClear.addEventListener('click',function(){
+    input.value='';
+})
+
+
+
+// divs.forEach(function(item){
+//     item.addEventListener('click',function(){
+//        item.remove();
+//     })
+// });
